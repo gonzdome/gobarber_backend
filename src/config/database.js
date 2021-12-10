a57e930065702c4edf626879a7e9ebc2d7b3d8c1
Gabriel Gonzalez Domeniconi <gabrielgonzalezdomeniconi@gmail.com>
2021-12-10 16:12:40 -0300

    database.js done

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,12 @@
 module.exports = {
     dialect: 'postgres',
+    username:'postgres',
+    host:'localhost',
+    database:'gobarber',
+    password:'docker',
+    define: {
+        timestamps:true,
+        underscored:true,
+        underscoredAll:true,
+    },
 };
