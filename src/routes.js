2df7c270e55a5ecb3ce4d480b2259e2e7d5f7925
Gabriel Gonzalez Domeniconi <gabrielgonzalezdomeniconi@gmail.com>
2021-12-13 11:21:50 -0300

    User model created, Model Loader created, addted, index.js changed, routes.js coded for testing, import added on app.js

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+    const user = await User.create({
+        name: 'Alberto Fonseca',
+        email: 'alberto.fonseca@gmail.com',
+        password_hash:'12341234',
+    });
+
+    return res.json(user);
+})
 
 export default routes;
